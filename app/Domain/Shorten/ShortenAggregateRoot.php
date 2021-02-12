200e2a1c3f61bfb343bcfdce152784dccee631d6
Diego <diego@diego.cat>
2021-02-12 20:43:33 +0100

    Refactors Shorten Create

--- app/Domain/Shorten/ShortenAggregateRoot.php
+++ app/Domain/Shorten/ShortenAggregateRoot.php
@@ -2,9 +2,7 @@
 
 namespace App\Domain\Shorten;
 
-use DomainException;
 use App\Domain\Shorten\Events\ShortenCreated;
-use Illuminate\Support\Facades\Validator;
 use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
 
 /**
@@ -17,19 +15,11 @@ class ShortenAggregateRoot extends AggregateRoot
 {
     public function createShorten(array $attributes): ShortenAggregateRoot
     {
-        $validator = Validator::make($attributes, [
-            'url' => 'required|url',
-            'slug' => 'required|unique:shortens,slug',
-            'hits' => 'integer',
-            'max_hits' => 'integer',
-            'expires_at' => 'date'
-        ]);
-
-        if ($validator->fails()) {
-            throw new DomainException();
-        }
-
         $this->recordThat(new ShortenCreated($attributes));
+
+        return $this;
+    }
+
 
         return $this;
     }
--- app/Http/Controllers/ShortenCreateController.php
+++ app/Http/Controllers/ShortenCreateController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Domain\Shorten\Events\ShortenCreated;
 use App\Domain\Shorten\ShortenAggregateRoot;
-use App\Http\Requests\CreateShortenRequest;
+use App\Http\Requests\ShortenCreateRequest;
 use App\Models\Shorten;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ShortenCreateController
 {
-    public function __invoke(CreateShortenRequest $request)
+    public function __invoke(ShortenCreateRequest $request)
     {
         $event = new ShortenCreated([
             'uuid' => Str::uuid()->toString(),
