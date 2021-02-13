78ed5eb75ae3ae6142191e4ad96dd0366cc94103
Diego <diego@diego.cat>
2021-02-13 12:53:47 +0100

    Adds tests for Shorten API hits

---
+++ tests/Feature/HitShortenAPITest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Database\Factories\ShortenFactory;
+use Illuminate\Foundation\Testing\{RefreshDatabase, WithFaker};
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class HitShortenAPITest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function test_hit_valid_shorten()
+    {
+        $shorten = ShortenFactory::new(['uuid' => Str::uuid()->toString()])->create();
+
+        $this
+            ->get(route('shorten.check', $shorten->slug))
+            ->assertStatus(JsonResponse::HTTP_OK)
+            ->assertSeeText('url')
+            ->assertDontSee('error');
+    }
+
+    /** @test */
+    public function test_hit_with_max_hits_unreached_shorten()
+    {
+        $shorten = ShortenFactory::new([
+            'uuid' => Str::uuid()->toString(),
+            'hits' => 100,
+            'max_hits' => 1000
+        ])->create();
+
+        $this
+            ->get(route('shorten.check', $shorten->slug))
+            ->assertStatus(JsonResponse::HTTP_OK)
+            ->assertDontSeeText('error');
+    }
+
+    /** @test */
+    public function test_hit_with_max_hits_reached_shorten()
+    {
+        $shorten = ShortenFactory::new([
+            'uuid' => Str::uuid()->toString(),
+            'hits' => 100,
+            'max_hits' => 10
+        ])->create();
+
+        $this
+            ->get(route('shorten.check', $shorten->slug))
+            ->assertStatus(JsonResponse::HTTP_FORBIDDEN)
+            ->assertSeeText('error');
+    }
+
+    /** @test */
+    public function test_hit_not_expired_shorten()
+    {
+        $shorten = ShortenFactory::new([
+            'uuid' => Str::uuid()->toString(),
+            'expires_at' => Carbon::tomorrow()->toDateTimeString()
+        ])->create();
+
+        $this
+            ->get(route('shorten.check', $shorten->slug))
+            ->assertStatus(JsonResponse::HTTP_OK)
+            ->assertDontSeeText('error');
+    }
+
+    /** @test */
+    public function test_hit_expired_shorten()
+    {
+        $shorten = ShortenFactory::new([
+            'uuid' => Str::uuid()->toString(),
+            'expires_at' => Carbon::yesterday()->toDateTimeString()
+        ])->create();
+
+        $this
+            ->get(route('shorten.check', $shorten->slug))
+            ->assertStatus(JsonResponse::HTTP_FORBIDDEN)
+            ->assertSeeText('error');
+    }
+}
