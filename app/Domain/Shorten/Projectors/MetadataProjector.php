b257ed9ae883cb37f9fc1d609d47ab5e9f13e07e
Diego <diego@diego.cat>
2021-02-13 15:47:16 +0100

    Adds metadata on events

---
+++ app/Domain/Shorten/Projectors/MetadataProjector.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Domain\Shorten\Projectors;
+
+use App\Domain\Shorten\Events\{ShortenCreated, ShortenHit, ShortenHitExpired, ShortenHitMaxReached, ShortenHitMissing};
+use Ramsey\Uuid\Uuid;
+use Spatie\EventSourcing\EventHandlers\Projectors\Projector;
+use Spatie\EventSourcing\Facades\Projectionist;
+use Spatie\EventSourcing\StoredEvents\Repositories\StoredEventRepository;
+use Spatie\EventSourcing\StoredEvents\StoredEvent;
+
+class MetadataProjector extends Projector
+{
+    public array $handlesEvents = [
+        ShortenCreated::class => 'addSourceIp',
+        ShortenHit::class => 'addSourceIp',
+        ShortenHitMissing::class => 'addSourceIp',
+        ShortenHitExpired::class => 'addSourceIp',
+        ShortenHitMaxReached::class => 'addSourceIp',
+    ];
+
+    public function addSourceIp(StoredEvent $storedEvent, StoredEventRepository $repository)
+    {
+        if (!Projectionist::isReplaying()) {
+            $storedEvent->meta_data = [
+                'url' => request()->getRequestUri(),
+                'method' => request()->getMethod(),
+                'source_ip' => request()->getClientIp(),
+                'useragent' => request()->userAgent(),
+                'body' => request()->all(),
+            ];
+
+            $storedEvent->meta_data['source_ip'] = request()->getClientIp();
+            $storedEvent->meta_data['useragent'] = request()->userAgent();
+
+            $repository->update($storedEvent);
+        }
+    }
+}
