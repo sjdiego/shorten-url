73a46ed3a8cbc5d9976469b417ff1ff34d3e1fdc
Diego <diego@diego.cat>
2021-02-14 12:06:03 +0100

    Add React and Tailwindcss config files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Shortener URL')
+
+@section('content')
+    <h1>Test</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => view('home'));
 
 Route::fallback(fn () => redirect('/'));
