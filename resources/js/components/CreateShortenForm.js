41036f41c7534cb49fdb7be20d280792d95da231
Diego <diego@diego.cat>
2021-02-15 12:53:11 +0100

    Adds AOS library

--- resources/js/components/CreateShortenForm.js
+++ resources/js/components/CreateShortenForm.js
@@ -48,27 +48,27 @@ export default class CreateShortenForm extends React.Component {
     render() {
         return (
             <div>
-                <div className="flex items-center justify-center px-5 py-5">
+                <div className="flex items-center justify-center px-5 py-5" data-aos="fade-up">
                     <form onSubmit={this.handleSubmit} className="w-full mx-auto rounded-lg bg-white shadow p-5 text-gray-800 max-w-screen-sm	">
                         <div className="relative mb-2">
                             <label className="block text-xs font-semibold text-gray-500 mb-2">URL</label>
-                            <input 
-                                placeholder="https://www.wikipedia.org" 
-                                autoComplete="off" 
+                            <input
+                                placeholder="https://www.wikipedia.org"
+                                autoComplete="off"
                                 onChange={this.handleSetUrl}
                                 required
-                                className="w-full pl-3 pr-10 py-2 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" 
+                                className="w-full pl-3 pr-10 py-2 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
                             />
                         </div>
-                        
+
                         <div className="mb-2 flex flex-row justify-between">
                             <ToggleButton />
                             <SubmitButton />
                         </div>
-                        
+
                         <div className="hidden" id="shortenerOptions">
                             <hr className="my-5 border border-gray-200" />
-                            
+
                             <div className="mb-2">
                                 <label className="block text-xs font-semibold text-gray-500 mb-2">MAX VISITS</label>
                                 <input onChange={this.handleSetMaxHits} className="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="0" type="number" min="1" step="1" />
