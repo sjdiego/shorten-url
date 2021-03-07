3816445333798263a9009132c59faef009a632b8
Diego <diego@diego.cat>
2021-03-07 18:39:01 +0100

    Get only latest 10 shortened URLs on Dashboard

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -7,14 +7,10 @@ use Inertia\Inertia;
 
 class BackendController
 {
-    const ITEMS_PER_PAGE = 10;
-
     public function render()
     {
         return Inertia::render('Dashboard', [
-            'shortens' => Shorten::select(['id', 'url', 'slug'])
-                ->get()
-                ->forPage(1, self::ITEMS_PER_PAGE)
+            'shortens' => Shorten::orderBy('id', 'desc')->take(10)->get()
         ]);
     }
 }
